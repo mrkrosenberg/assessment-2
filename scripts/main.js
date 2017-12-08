1fec44d15c74fade9de51938acc92d0e41a4abf4
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-12-07 19:40:37 -0700

    adds racer vars

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,33 @@
 $(document).ready( function() {
 	console.log("shit works!");
 
-	
+	var racer1 = $('#racer1').position();
+	var racer2 = $('#racer2').position();
 
+	console.log(racer1.left);
+	console.log(racer2.left);
 
 	$(document).keydown( function(race) {
 		if (race.keyCode == 39) {
 			$('#racer1').animate( {
-				marginLeft: '+=10'
+				marginLeft: '+=10vw'
 			});
 		} else if (race.keyCode == 190) {
 			$('#racer2').animate( {
-				marginLeft: '+=10'
+				marginLeft: '+=10vw'
 			});
 		}
 	});
+	
+	var winner = function () {
+		if (racer1 = 8) {
+			alert("Racer 1 Wins!");
+		} else if (racer2 = 8) {
+			alert("Racer 2 Wins!");
+		}
+	};
+
+
 
 
 
