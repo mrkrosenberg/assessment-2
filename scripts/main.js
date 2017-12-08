c9cc6ae3649e0927333cc74c228dee131a627e6f
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-12-07 18:43:03 -0700

    creates race function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
 $(document).ready( function() {
 	console.log("shit works!");
 
+	
+
+
+	$(document).keydown( function(race) {
+		if (race.keyCode == 39) {
+			$('#racer1').animate( {
+				marginLeft: '+=10'
+			});
+		} else if (race.keyCode == 190) {
+			$('#racer2').animate( {
+				marginLeft: '+=10'
+			});
+		}
+	});
 
 
 
@@ -9,11 +23,6 @@ $(document).ready( function() {
 
 
 
+});
 
-
-
-
-
-
-
-} );
+console.log("wooo");
